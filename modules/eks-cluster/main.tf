d8e29b2e43e8b359032d7e2f1bd4ff815bf39971
Sallyhenz <sallyhenz@gmail.com>
2024-11-04 17:00:20 +0100

    modified region in eks module

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -65,7 +65,7 @@ module "eks" {
   aws_auth_roles = [
     {
       rolearn  = var.rolearn
-      username = "ayo-IAM"
+      username = "sally-devops"
       groups   = ["system:masters"]
     },
   ]
